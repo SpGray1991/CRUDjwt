6acec803bd3c17f8d4ff30448171423fd6058c71
Gray.1991 <Sergey.sp.1991@gmail.com>
2022-04-22 20:52:59 +0300

    crud ts

--- authController.ts
+++ authController.ts
@@ -5,8 +5,9 @@ import jwt from "jsonwebtoken";
 import secret from "./config/config";
 import { validationResult } from "express-validator";
 import log from "./config/winston";
+import { Request, Response } from "express";
 
-const generateAccessToken = (id, roles) => {
+const generateAccessToken = (id: string, roles: string) => {
   const payload = {
     id,
     roles,
@@ -15,26 +16,32 @@ const generateAccessToken = (id, roles) => {
 };
 
 class authController {
-  async registration(req, res) {
+  async registration(req: Request, res: Response) {
     try {
       const errors = validationResult(req);
+
       if (!errors.isEmpty()) {
         return res.status(400).json({ message: "Validate error", errors });
       }
+
       const { username, password } = req.body;
-      const candidate = await User.findOne({ username });
+      const candidate: string = await User.findOne({ username });
+
       if (candidate) {
         return res
           .status(400)
           .json({ message: "Пользователь с таким именем уже существует" });
       }
-      const hashPassword = bcrypt.hashSync(password, 7);
+
+      const hashPassword: string = bcrypt.hashSync(password, 7);
       const userRole = await Role.findOne({ value: "USER" });
+
       const user = new User({
         username,
         password: hashPassword,
         roles: [userRole.value],
       });
+
       await user.save();
       return res.json({ message: "Пользователь успешно зарегистрирован" });
     } catch (e) {
@@ -42,9 +49,11 @@ class authController {
       res.status(400).json({ message: "Registration error" });
     }
   }
-  async login(req, res) {
+
+  async login(req: Request, res: Response) {
     try {
-      const { username, password } = req.body;
+      const username: string = req.body;
+      const password: string = req.body;
 
       const user = await User.findOne({ username });
 
@@ -54,7 +63,7 @@ class authController {
           .json({ message: `Пользователь ${username} не найден` });
       }
 
-      const validPassword = bcrypt.compareSync(password, user.password);
+      const validPassword: string = bcrypt.compareSync(password, user.password);
 
       if (!validPassword) {
         return res.status(400).json({ message: `Введен не верный пароль` });
@@ -68,7 +77,8 @@ class authController {
       res.status(400).json({ message: "Login error" });
     }
   }
-  async getUsers(req, res) {
+
+  async getUsers(req: Request, res: Response) {
     try {
       const users = await User.find();
       res.json(users);
@@ -77,25 +87,27 @@ class authController {
     }
   }
 
-  async update(req, res) {
+  async update(req: Request, res: Response) {
     try {
-      console.log("=====", req.body);
-      const updatedUser = await User.findByIdAndUpdate(req.body._id, req.body, {
-        new: true,
-      });
+      const updatedUser: string = await User.findByIdAndUpdate(
+        req.body._id,
+        req.body,
+        {
+          new: true,
+        }
+      );
       if (!req.body._id) {
         return res.status(400).json({ message: `Введите id` });
       }
-
       return res.json(updatedUser);
     } catch (e) {
       res.status(500).json(e.message);
     }
   }
 
-  async delete(req, res) {
+  async delete(req: Request, res: Response) {
     try {
-      const user = await User.findByIdAndRemove(req.params.id);
+      const user: string = await User.findByIdAndRemove(req.params.id);
       if (!req.params.id) {
         return res.status(400).json({ message: `Введите id` });
       }
